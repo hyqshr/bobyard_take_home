3d0db2a542c7d71c561d94ae319280deede9f7ba
Yiqiu <hyiqiu23@gmail.com>
2023-12-31 16:47:24 -0500

    get rid of unnessary code

--- backend/api/serializers/comment_serializer.py
+++ backend/api/serializers/comment_serializer.py
@@ -3,12 +3,7 @@ from rest_framework import serializers
 from ..models import Comment
 
 class CommentSerializer(serializers.ModelSerializer):
-    replies = serializers.SerializerMethodField()
-    
     class Meta:
         model = Comment
         fields = ['id', 'author', 'text', 'date', 'likes', 'image', 'parent', 'replies']
         
-    def get_replies(self, obj):
-        # This will be handled in the viewset
-        return []
